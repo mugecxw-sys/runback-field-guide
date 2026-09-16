599130cc2cc09fbc802946b19fdc2013be557d7e
mugecxw-sys <mugecxw@gmail.com>
2026-09-16 20:21:09 +0800

    Ensure enemy dossiers stay visible on desktop

--- components/enemy-index.tsx
+++ components/enemy-index.tsx
@@ -4,7 +4,7 @@ import { repoEnemies, type RepoEnemy } from '@/lib/repo-enemies';
 
 function Dossier({ enemy }: { enemy: RepoEnemy }) {
   return (
-    <div className="hidden group-open:block md:block">
+    <div className="hidden group-open:block md:!block">
       <img
         src={enemy.image}
         alt={enemy.name + ' reference'}
